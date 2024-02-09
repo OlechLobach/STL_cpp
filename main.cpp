df05fadaffedea47db69db7596d5db8dfd499489
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-02-09 02:31:02 +0200

    initiall commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-#include<iostream>
+#include"stl.h"
 
-int main()
-{
+int main() {
+    Dictionary dictionary;
 
-	return 0;
+    dictionary.addWord("apple", "a round fruit with red or green skin");
+    dictionary.addWord("banana", "a long curved fruit with a yellow skin");
+    dictionary.addWord("cat", "a small domesticated carnivorous mammal");
+
+    dictionary.displayAllWords();
+
+    string searchWord;
+    cout << "Enter a word to search: ";
+    cin >> searchWord;
+    cout << dictionary.searchWord(searchWord) << endl;
+
+    string editWord, newDefinition;
+    cout << "Enter a word to edit: ";
+    cin >> editWord;
+    cout << "Enter the new definition: ";
+    cin.ignore(); 
+    getline(cin, newDefinition);
+    dictionary.editDefinition(editWord, newDefinition);
+
+    string removeWord;
+    cout << "Enter a word to remove: ";
+    cin >> removeWord;
+    dictionary.removeWord(removeWord);
+
+    dictionary.saveToFile("dictionary.txt");
+
+    Dictionary newDictionary;
+    newDictionary.loadFromFile("dictionary.txt");
+    newDictionary.displayAllWords();
+
+    return 0;
 }
--- stl.h
+++ stl.h
@@ -1,4 +1,79 @@
 #ifndef stl_h
 #define stl_h
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
+using namespace std;
 
+class Dictionary {
+private:
+    map<string, string> words;
+
+public:
+    void addWord(const string& word, const string& definition) {
+        words[word] = definition;
+    }
+
+    void removeWord(const string& word) {
+        auto it = words.find(word);
+        if (it != words.end()) {
+            words.erase(it);
+        }
+    }
+
+    void editDefinition(const string& word, const string& newDefinition) {
+        auto it = words.find(word);
+        if (it != words.end()) {
+            it->second = newDefinition;
+        }
+    }
+
+    string searchWord(const string& word) const {
+        auto it = words.find(word);
+        if (it != words.end()) {
+            return it->second;
+        }
+        return "Word not found.";
+    }
+
+    void displayAllWords() const {
+        cout << "Dictionary:" << endl;
+        for (const auto& pair : words) {
+            cout << pair.first << ": " << pair.second << endl;
+        }
+    }
+
+    void saveToFile(const string& filename) const {
+        ofstream file(filename);
+        if (file.is_open()) {
+            for (const auto& pair : words) {
+                file << pair.first << "," << pair.second << endl;
+            }
+            cout << "Dictionary saved to file." << endl;
+        }
+        else {
+            cerr << "Unable to open file for writing." << endl;
+        }
+    }
+
+    void loadFromFile(const string& filename) {
+        ifstream file(filename);
+        if (file.is_open()) {
+            string line;
+            while (getline(file, line)) {
+                size_t pos = line.find(',');
+                if (pos != string::npos) {
+                    string word = line.substr(0, pos);
+                    string definition = line.substr(pos + 1);
+                    words[word] = definition;
+                }
+            }
+            cout << "Dictionary loaded from file." << endl;
+        }
+        else {
+            cerr << "Unable to open file for reading." << endl;
+        }
+    }
+};
 #endif
