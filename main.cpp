736db132e5cb787972e43237b7e81ebcb6be5a92
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-02-09 02:15:26 +0200

    initiall commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
-#include<iostream>
+#include"stl.h"
 
-int main()
-{
+int main() {
+    TrainStation station;
 
-	return 0;
+    char addAnother;
+    do {
+        string number, depTime, destStation;
+        cout << "Enter train number: ";
+        cin >> number;
+        cout << "Enter departure time: ";
+        cin >> depTime;
+        cout << "Enter destination station: ";
+        cin >> destStation;
+
+        station.addTrain(number, depTime, destStation);
+
+        cout << "Do you want to add another train? (y/n): ";
+        cin >> addAnother;
+    } while (addAnother == 'y' || addAnother == 'Y');
+
+    station.displayAllTrains();
+
+    string searchNumber;
+    cout << "Enter the train number to search: ";
+    cin >> searchNumber;
+    station.searchTrain(searchNumber);
+
+    return 0;
 }
--- stl.h
+++ stl.h
@@ -1,4 +1,58 @@
 #ifndef stl_h
 #define stl_h
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+class Train {
+private:
+    string number;
+    string departureTime;
+    string destinationStation;
+
+public:
+    Train(const string& num, const string& depTime, const string& destStation)
+        : number(num), departureTime(depTime), destinationStation(destStation) {}
+
+    void display() const {
+        cout << "Train number: " << number << ", Departure time: " << departureTime
+            << ", Destination station: " << destinationStation << endl;
+    }
+
+    const string& getNumber() const {
+        return number;
+    }
+};
+
+class TrainStation {
+private:
+    vector<Train> trains;
+
+public:
+    void addTrain(const string& num, const string& depTime, const string& destStation) {
+        trains.push_back(Train(num, depTime, destStation));
+    }
+
+    void displayAllTrains() const {
+        cout << "All trains at the station:" << endl;
+        for (const auto& train : trains) {
+            train.display();
+        }
+    }
+
+    void searchTrain(const string& trainNumber) const {
+        bool found = false;
+        for (const auto& train : trains) {
+            if (train.getNumber() == trainNumber) {
+                found = true;
+                train.display();
+                break;
+            }
+        }
+        if (!found) {
+            cout << "Train with number " << trainNumber << " not found." << endl;
+        }
+    }
+};
 #endif
